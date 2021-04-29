62216f1b4a6587e98caac63ad71159e2872a0822
Abdulrahman <aalsoheim@gmail.com>
2021-04-29 15:23:35 +0300

    replace deprecated postgres with postgresql

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ DEBUG = True
 database_path = os.environ.get('DATABASE_URL')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_PATH')
 if database_path:
-    SQLALCHEMY_DATABASE_URI = database_path
+    SQLALCHEMY_DATABASE_URI = database_path.replace('postgres', 'postgresql')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
